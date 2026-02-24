c43e38c97bc823936fbacfd4d450ec6ff58f2d55
taeef-ahmed <taifahmed782@gmail.com>
2026-02-24 17:06:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if(top>offset && top < offset + height){
+            sec.classList.add('start-animation');
+            navlink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY>100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
+} 
+const texts = [
+  "Frontend Developer",
+  "Web Developer",
+  "Student",
+  "Learning Ethical Hacking"
+];
+
+let index = 0;
+const textElement = document.getElementById("changing-text");
+
+function changeText() {
+  index = (index + 1) % texts.length;
+  textElement.textContent = texts[index];
+}
+
+setInterval(changeText, 2000); // 2 seconds পর change হবে
